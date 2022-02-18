24c02f3804661adbfd3b1fbf821d6a81a3417b0e
Nadir Hierro <nadir.hierro@gmail.com>
2022-02-17 22:58:00 -0300

    Menú

--- assets/js/height.js
+++
@@ -1,8 +0,0 @@
-const height = document.querySelector(".servicios").offsetHeight;
-console.log(height);
-
-const toMove = document.querySelectorAll(".toMove");
-
-toMove.forEach((element) => {
-  element.style.setProperty.top = `${height}`;
-});
